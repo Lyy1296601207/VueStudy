dd2b6978ab0a4b9db41178353b7a0307e4bec1cb
Absalom <1296601207@qq.com>
2020-12-03 17:08:27 +0800

    初始化vue工程，项目提交

---
+++ lyy-project/src/App.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: yangyang.liu
+ * @Date: 2020-12-03 16:57:56
+ * @LastEditors: yangyang.liu
+ * @LastEditTime: 2020-12-03 17:07:18
+-->
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
